e465cc3263aaec14698212b0c42472afcc0d0518
Alderum <alderumplay@gmail.com>
2022-08-11 15:54:20 +0300

    Added MySQL database

---
+++ DataBase.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace PasswordCreator
+{
+    class DataBase
+    {
+        public string utils;
+
+        private void CommandExecutor(string text)
+        {
+
+            MySqlConnection connection = new MySqlConnection(utils);
+
+            MySqlCommand command = new MySqlCommand(text, connection);
+
+            MySqlDataReader reader;
+            connection.Open();
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+            }
+            connection.Close();
+        }
+
+
+        public void Save(string password, string name)
+        {
+            try
+            {
+                //Command for execute
+                string insert = "insert into pascr_sql.list(pass, name)" +
+                               "values('" + password + "','" + name + "');";
+
+                //Execute command insert
+                CommandExecutor(insert);
+                Console.WriteLine("Password saved");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        public void Delete(string name)
+        {
+            try
+            {
+                //Command for execute
+                string delete = "delete from pascr_sql.list where name = '" + name + "';";
+
+                //Execute command delete
+                CommandExecutor(delete);
+                Console.WriteLine("Password deleted");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void Show()
+        {
+            //Command for execute
+            string text = "SELECT pass, name FROM pascr_sql.list";
+            MySqlConnection connection = new MySqlConnection(utils);
+
+            MySqlCommand command = new MySqlCommand(text, connection);
+
+            MySqlDataReader reader;
+            connection.Open();
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                string pass = (string)reader["pass"];
+                string name = (string)reader["name"];
+
+                // Print the data.
+                Console.WriteLine(name + " " + pass);
+            }
+            connection.Close();
+        }
+
+        public void ShowByName(string name)
+        {
+            //Command for execute
+            string text = "SELECT pass, name FROM pascr_sql.list where name = '" + name + "';";
+            MySqlConnection connection = new MySqlConnection(utils);
+
+            MySqlCommand command = new MySqlCommand(text, connection);
+
+            MySqlDataReader reader;
+            connection.Open();
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                string pass = (string)reader["pass"];
+
+                // Print the data.
+                Console.WriteLine(name + " " + pass);
+            }
+            connection.Close();
+        }
+
+
+        public void Update(string name, string pass)
+        {
+            string update = "update pascr_sql.list set pass = '" + pass + "' where name='" + name + "';";
+
+            //Execute command update
+            CommandExecutor(update);
+            Console.WriteLine("Password whith name: " + name + " was updated to: " + pass);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PasswordCreator;
 
 namespace Passwordcreator
 {
@@ -16,35 +17,38 @@ namespace Passwordcreator
             StringBuilder value = new StringBuilder();
 
             Random random = new Random();
+            DataBase dataBase = new DataBase();
+            //Utils to MySQL
+            dataBase.utils = "datasource=localhost;port=3306;username=root;password=" + AddPassword();
 
             string english = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
             string ukraine = "йцукенгшщзхїфівапролджєячсмитьбю";
             string punctuation = "[]{};:',<.>/?-_=+|!#@$%^&*()~`";
             string number = "1234567890";
-
             do
             {
                 value.Clear();
 
                 text = Console.ReadLine();
 
-                if (text == "create password")
+                if (text.Contains("create password"))
                 {
                     //Password settings
                     Console.Write("Password setigs: " +
-                        "\nThis program can create password only with three language Ukraine English" + 
+                        "\nThis program can create password only with three language Ukraine English" +
                         "\nand punctuation simbols." +
                         "\nPassword width: ");
 
                     int pw = Convert.ToInt32(Console.ReadLine());
-                    
-                    Console.Write("Language: Ukraine, English" + 
+
+                    Console.Write("Language: Ukraine, English" +
                         "\nOther simbols: Punctuation, Number" +
                         "\nWhat languages or punctuation do you want to be in your password: ");
 
                     language = Console.ReadLine();
                     Console.WriteLine("Your password: ");
 
+                    //Add lenguage by name to value
                     AddLenguages(language, value, english, ukraine, punctuation, number);
 
 
@@ -54,16 +58,57 @@ namespace Passwordcreator
                         password = password + value[random.Next(0, value.Length)];
 
                     Console.WriteLine(password);
+
+                    if (text.Contains("save"))
+                    {
+                        Console.WriteLine("Enter name for your password: ");
+                        string name = Console.ReadLine();
+
+                        //Save password with name
+                        dataBase.Save(password, name);
+                    }
+
+                }
+                else if(text == "delete password")
+                {
+                    Console.WriteLine("Enter name of the password for delete: ");
+
+                    string name = Console.ReadLine();
+                    //Delete password by name
+                    dataBase.Delete(name);
+                }
+                else if(text == "show passwords")
+                {
+                    dataBase.Show();
+                }
+                else if(text.Contains("show password by name: "))
+                {
+                    //Extract passwords name
+                    text = text.Remove(0, 23);
+                    //Show password by name (text)
+                    dataBase.ShowByName(text);
+                }
+                else if (text.Contains("update password"))
+                {
+                    Console.WriteLine("Name of password wich you want to update: ");
+                    string name = Console.ReadLine();
+
+                    Console.WriteLine("New password: ");
+                    string pass = Console.ReadLine();
+
+                    //Update password by name to pass
+                    dataBase.Update(name, pass);
                 }
                 else
                     Console.WriteLine("To create password write: create password");
 
             } while (text != "Stop");
 
+
             return;
 
             //End of function
-        }
+            }
         
         static void AddLenguages(string language, StringBuilder value, string english, string ukraine, string punctuation, string number)
         {
@@ -78,6 +123,12 @@ namespace Passwordcreator
 
             if (language.Contains("Punctuation"))
                 value.Append(punctuation);
+        }
+
+        private static string AddPassword()
+        {
+            Console.Write("Password: ");
+            return Console.ReadLine();
         }
     }
 }
